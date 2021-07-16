965d5feb8958bad7737752e948dae519c9145777
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-07-16 20:15:52 -0300

    Create Services and Contact

--- main.js
+++ main.js
@@ -30,3 +30,30 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scroll')
     }
 })
+
+// Testimonials carousel Swiper
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    Keyboard: true,
+  });
+
+// ScrollReveal: mostar elementos quando der scroll na pagination
+const  scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+   `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+    `, { interval: 100 }
+);
