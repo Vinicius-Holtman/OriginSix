8e2c6eb0ee3a89b84c1dee55f2b8014c24dbb486
Vinicius Holtman <42218598+Vinicius-Holtman@users.noreply.github.com>
2021-07-15 21:45:54 -0300

    Create about and services

---
+++ main.js
@@ -0,0 +1,32 @@
+// abre e fecha o menu
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for(const element of toggle) {
+    element.addEventListener('click', () => {
+        nav.classList.toggle('show')
+    })
+}
+
+// quando clicar em um item no menu, esconder o menu
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+    link.addEventListener('click', () => {
+        nav.classList.remove('show')
+    })
+}
+
+// colocar sombra no header quando der scroll
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY >= navHeight) {
+        // maior que a altura do header
+        header.classList.add('scroll')
+    } else {
+        // meno que a altura do header
+        header.classList.remove('scroll')
+    }
+})
